c9e1b064cd36ce1cd902c05abcd16ab04c48d129
realSanele <therealsanele@gmail.com>
2018-10-15 12:03:12 -0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,66 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  minQty = 0;
+  value: number;
+  uname: '';
+  total= 58.99;
+  wCream:string;
+  choco: string;
+  quantity: number;
+  whipped_cream: boolean;
+  chocolate: boolean;
 
   constructor(public navCtrl: NavController) {
+    this.quantity = 0;
 
+    this.whipped_cream = false;
+    this.chocolate = false;
+  }
+
+  /*checkBox(){
+    if(this.chocolate){
+      this.whipped_cream = false;
+    }else if(this.whipped_cream){
+      this.chocolate = false;
+    }
+
+    if(this.whipped_cream){
+      this.chocolate = false;
+    }else if(this.chocolate){
+      this.whipped_cream = false;
+    }
+
+  }*/
+
+  incrementQty(){
+    this.quantity += 1;
+  }
+
+  decrementQty(){
+    if(this.quantity > this.minQty){
+      this.quantity -= 1;
+    }
+    
+  }
+
+  order(){
+    if(this.whipped_cream){
+      this.wCream = '+Whipped Cream';
+    }
+
+    if(this.chocolate){
+      this.choco ='+Chocolate';
+    }
+    this.value = 1;
+  }
+
+  reset(){
+    this.value = 0;
+    this.quantity = 0;
+    this.uname = '';
+    this.whipped_cream = false;
+    this.chocolate = false;
   }
 
 }
